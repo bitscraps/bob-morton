f71bd161c407d7eb4160a6ecb345492071bffa54
Graham Hadgraft <graham.hadgraft@gmail.com>
2017-11-27 19:52:49 +0000

    remove refile

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,5 @@
 require 'sidekiq'
 require 'sidekiq/web'
-require 'sofar/refile'
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == [ENV['SIDEKIQ_USERNAME'], ENV['SIDEKIQ_PASSWORD']]
