8b846f478cd521b7e3003dadb9ecbd2a3751a3e3
Graham Hadgraft <graham.hadgraft@gmail.com>
2018-05-21 22:29:58 +0100

    Allow rendering if no brakeman warnings

--- app/controllers/patch_controller.rb
+++ app/controllers/patch_controller.rb
@@ -6,6 +6,9 @@ class PatchController < ApplicationController
     @response = client.pull_files("#{params[:user]}/#{params[:repo]}", params[:pr_number])
 
     @rubocop_warnings = Commit.where(number: params[:pr_number]).last.rubocop_output.split("\n")
-    @brakeman_warnings = JSON.parse(Commit.where(number: params[:pr_number]).last.brakeman_output)
+    brakeman_warnings = Commit.where(number: params[:pr_number]).last
+    if brakeman_warnings
+      @brakeman_warnings = JSON.parse(brakeman_warnings.brakeman_output)
+    end
   end
 end
